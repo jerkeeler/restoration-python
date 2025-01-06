ba61e27856a5dc853c00cdfe582e8eafef9eea58
Jeremy Keeler <jeremy@keeler.dev>
2025-01-05 21:42:24 -0500

    start adding refiners

--- restoration/game_commands.py
+++ restoration/game_commands.py
@@ -1,8 +1,9 @@
 import logging
 from dataclasses import dataclass, field
+from typing import Callable
 
 from restoration.consts import FOOTER
-from restoration.utils import read_int32, read_short
+from restoration.utils import read_int32, read_short, read_uint32
 
 logger = logging.getLogger(__name__)
 
@@ -18,6 +19,105 @@ class CommandList:
     commands: list[CommandItem] = field(default_factory=list)
 
 
+def unpack_int32():
+    return 4
+
+
+def unpack_int8():
+    return 1
+
+
+def unpack_vector():
+    return 12
+
+
+def unpack_float():
+    return 4
+
+
+REFINERS: dict[int, list[Callable[[], int]]] = {
+    0: [
+        unpack_int32,
+        unpack_int32,
+        unpack_int32,
+        unpack_int32,
+        unpack_vector,
+        unpack_float,
+        unpack_int32,
+        unpack_int32,
+        unpack_int32,
+    ],
+    1: [unpack_int32, unpack_int32, unpack_int32],
+    2: [unpack_int32, unpack_int32, unpack_int32, unpack_int32, unpack_int8, unpack_int8],
+    3: [
+        unpack_int32,
+        unpack_int32,
+        unpack_int32,
+        unpack_vector,
+        unpack_int32,
+        unpack_int32,
+        unpack_float,
+        unpack_int32,
+        unpack_int32,
+        unpack_int32,
+        unpack_int32,
+    ],
+    4: [unpack_int32, unpack_int32, unpack_vector, unpack_float, unpack_int32, unpack_int32],
+    7: [unpack_int32, unpack_int32, unpack_int8],
+    9: [unpack_int32, unpack_int32],
+    12: [
+        unpack_int32,
+        unpack_int32,
+        unpack_int32,
+        unpack_vector,
+        unpack_vector,
+        unpack_int32,
+        unpack_int32,
+        unpack_float,
+        unpack_int32,
+        unpack_int32,
+        unpack_int8,
+    ],
+    13: [unpack_int32, unpack_int32, unpack_int32, unpack_float],
+    14: [unpack_int32, unpack_int32],
+    18: [unpack_int32, unpack_int32, unpack_int32],
+    19: [unpack_int32, unpack_int32, unpack_int32, unpack_int32, unpack_float, unpack_float, unpack_int8],
+    23: [unpack_int32, unpack_int32, unpack_int32, unpack_int8, unpack_int8],
+    25: [unpack_int32, unpack_int32, unpack_int8, unpack_int8, unpack_int32],
+    26: [unpack_int32, unpack_int32, unpack_int8, unpack_int32],
+    34: [unpack_int32, unpack_int32],
+    35: [unpack_int32, unpack_int32, unpack_int32],
+    37: [unpack_int32, unpack_int32, unpack_int8, unpack_int32],
+    38: [unpack_int32, unpack_int32, unpack_int32],
+    41: [
+        unpack_int32,
+        unpack_int32,
+        unpack_int32,
+        unpack_int32,
+        unpack_int32,
+        unpack_int32,
+        unpack_int32,
+        unpack_int32,
+        unpack_int32,
+        unpack_int32,
+        unpack_int32,
+        unpack_int8,
+    ],
+    44: [unpack_int32, unpack_int32, unpack_int32, unpack_int32],
+    45: [unpack_int32, unpack_int32, unpack_int32, unpack_int32, unpack_int32],
+    48: [unpack_int32, unpack_int32, unpack_int32, unpack_int32],
+    53: [unpack_int32, unpack_int32, unpack_int32],
+    55: [unpack_int32, unpack_int32, unpack_vector],
+    66: [unpack_int32, unpack_int32, unpack_int32],
+    67: [unpack_int32, unpack_int32, unpack_int8],
+    68: [unpack_int32, unpack_int32, unpack_vector, unpack_vector],
+    69: [unpack_int32, unpack_int32, unpack_int32, unpack_vector, unpack_vector],
+    71: [unpack_int32, unpack_int32],
+    72: [unpack_int8, unpack_int32, unpack_int32, unpack_int8],
+    75: [unpack_int32, unpack_int32, unpack_int32, unpack_int32],
+}
+
+
 def parse_command_list(data: bytes, header_end_offset: int) -> list[CommandList]:
     logger.debug(f"Parsing command list starting at {header_end_offset=}")
     offset = data[header_end_offset:].find(FOOTER)
@@ -31,9 +131,10 @@ def parse_command_list(data: bytes, header_end_offset: int) -> list[CommandList]
     command_list: list[CommandList] = []
 
     while True:
-        if offset == len(data):
+        if offset == len(data) - 1:
             break
         item = parse_item(data, offset)
+        command_list.append(item)
         last_index += 1
         offset = item.offset_end
 
@@ -52,7 +153,7 @@ def parse_footer(data: bytes, offset: int) -> int:
 
     unk = data[offset]
     if unk != 1:
-        raise Exception(f"Expected unk to equal 1 at {offset=} by got {unk=}")
+        raise Exception(f"Expected unk to equal 1 at {offset=} but got {unk=}")
 
     offset += 9
     one_fourth_footer_length = read_short(data, offset)
@@ -72,13 +173,13 @@ def parse_item(data: bytes, offset: int) -> CommandList:
     128
     """
     logger.debug(f"Parsing item at {offset=}")
-    entry_type = read_short(data, offset)
+    entry_type = read_uint32(data, offset)
     offset += 4
     early_byte = data[offset]
     offset += 1
 
     # Raise exceptions if the bit mask is not valid
-    logger.debug(f"{offset=} {entry_type}")
+    logger.debug(f"{offset=} {entry_type=}")
     if entry_type & 225 != entry_type:
         raise Exception(f"Bad entry type, masking to 225 doesn't work for {entry_type}")
     # 32 + 64 together makes no sense
@@ -99,7 +200,7 @@ def parse_item(data: bytes, offset: int) -> CommandList:
             num_items = data[offset]
             offset += 1
         elif entry_type & 64:
-            num_items = read_short(data, offset)
+            num_items = read_uint32(data, offset)
             offset += 4
 
         for _ in range(num_items):
@@ -114,12 +215,12 @@ def parse_item(data: bytes, offset: int) -> CommandList:
         num_items = data[offset]
         offset += 1
         for _ in range(num_items):
-            selected_units.append(read_short(data, offset))
+            selected_units.append(read_uint32(data, offset))
             offset += 4
 
     footer_end_offset = parse_footer(data, offset)
     offset = footer_end_offset
-    entry_index = read_short(data, offset)
+    entry_index = read_uint32(data, offset)
     offset += 4
     finalByte = data[offset]
     offset += 1
@@ -129,14 +230,15 @@ def parse_item(data: bytes, offset: int) -> CommandList:
 
 def parse_game_command(data, offset) -> CommandItem:
     command_type = data[offset + 1]
+    logger.debug(f"{command_type=}")
     ten_bytes_offset = offset
-    offset += 1
+    offset += 10
     if command_type == 14:
         offset += 20
     else:
         offset += 8
 
-    three = read_short(data, offset)
+    three = read_uint32(data, offset)
     if three != 3:
         raise Exception("Expecting three in parsing game command!")
     offset += 4
@@ -178,6 +280,10 @@ def parse_game_command(data, offset) -> CommandItem:
     offset += num_pre_argument_bytes
 
     # Refiners
-    raise Exception(f"Need to add refiner for command_type {command_type}")
+    refiner = REFINERS.get(command_type)
+    if not refiner:
+        raise Exception(f"Need to add refiner for command_type {command_type}")
+    for func in refiner:
+        offset += func()
 
     return CommandItem(offset)
